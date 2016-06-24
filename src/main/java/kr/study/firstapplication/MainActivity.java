66af88b349d0372710d2dac39152464e9cb1cb6d
songjuyoung <vhehdvhehdnv@naver.com>
2016-06-24 10:28:52 +0900

    inint

---
+++ src/main/java/kr/study/firstapplication/MainActivity.java
@@ -0,0 +1,31 @@
+package kr.study.firstapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        TextView textView = (TextView)findViewById(R.id.text);
+
+        String s = calc(100);
+        s += calc(1000);
+        s += calc(10000);
+        s += calc(500);
+
+        textView.setText(s);
+    }
+
+    private String calc(int n){
+        int a = 0;
+        for(int i=1; i<=n;i++){
+            a+=i;
+        }
+        String s = "1부터 " + String.valueOf(n) + "을 더한 값은 " + String.valueOf(a) + "\n";
+        return s;
+    }
+}
